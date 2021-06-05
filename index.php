aac49bfad7d8ba015d435e3388ddad986a44633e
Marius Kouassi <45425563+kkmarius@users.noreply.github.com>
2021-06-05 19:14:32 +0000

    Firstcommit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+
+	<title>Hash password </title>
+	<meta name="description" content="Hash password">
+	<meta name="author" content="Hash password">
+
+</head>
+
+<body>
+	<h1>Mot de passe avec un Hash</h1>
+	<form action="" method="post">
+		<fieldset>
+			<label id="password">Mot de passe :</label>
+			<input type="text" name="password" placeholder="mot de passe à hasher">
+			<input type="submit" name="valide" value="Hasher">
+		</fieldset>
+
+	</form>
+	<!-- On fait le hasahge avec BCRYPT par défaut dans la fonction password_hash de php -->
+
+	<?php
+	if (isset($_POST['valide'])) : ?>
+		<p>
+			Vous avez rentré : <span id="origin_mot"><?php echo $_POST['password']; ?></span>
+		</p>
+		<p>
+			Le mot après hasahage est :
+			<span id="hash_mot"><?php echo password_hash($_POST['password'], PASSWORD_DEFAULT); ?></span> <button id="copy">Copiez !</button>
+
+		</p>
+	<?php endif; ?>
+
+	<script>
+		function copy() {
+			var textToCopy = document.getElementById("hash_mot").innerHTML;
+			navigator.clipboard.writeText(textToCopy)
+				.then(() => {
+					alert(`Texte copié !`)
+				})
+				.catch((error) => {
+					alert(`Copy failed! ${error}`)
+				})
+		}
+		document.querySelector("#copy").addEventListener("click", copy);
+	</script>
+</body>
+
+</html>
